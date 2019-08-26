270cba50d1eb8fe8738389a391f3291fc9f942fc
chenguanxi <chenguanxi@corp.netease.com>
2019-08-26 19:56:37 +0800

    feat: adding th library

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var starWarsNames = require('./starwars-names.json')
+var uniqueRandomArray = require('unique-random-array')
+module.exports = {
+    all: starWarsNames,
+    random: uniqueRandomArray(starWarsNames)
+}
