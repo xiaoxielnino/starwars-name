c357be2b5b3c117ee38887f6117ecc6274777bd0
chenguanxi <chenguanxi@corp.netease.com>
2019-08-26 20:40:17 +0800

    feat: add unit test

---
+++ src/index.test.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+var starwars = require('./index')
+
+describe('starwars-names', function() {
+    describe('all', () => {
+        it('should be an array of string', () => {
+            expect(starwars.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every((item)=> {
+                    return typeof item === 'string'
+                })
+            }
+        })
+        it('should contain chris', () => {
+            expect(starwars.all).to.include('Chirs(test)')
+        })
+    });
+    describe('random', () => {
+        it('should return a random name ', () => {
+            var randomItem = starwars.random();
+            expect(starwars.all).to.include(randomItem)
+        })
+    })
+    
+})
