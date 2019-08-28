89f566d7ae82fa16eb612f19d7406a19466a6632
chenguanxi <chenguanxi@corp.netease.com>
2019-08-28 19:36:36 +0800

    feat: add ghooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-var starWarsNames = require('./starwars-names.json')
-var uniqueRandomArray = require('unique-random-array')
+import uniqueRandomArray from 'unique-random-array';
+import starWarsNames from './starwars-names.json';
+
+const getRandomItem = uniqueRandomArray(starWarsNames);
+
 module.exports = {
-    all: starWarsNames,
-    random: uniqueRandomArray(starWarsNames)
+  all: starWarsNames,
+  random: random
+};
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    const randomItems = [];
+    for (var i = 1; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
 }
